cdade88947351bb884be44c85ae00a3c3ce50d5a
Mauricio Faustino <mauricio.faustino@gmail.com>
2018-04-05 20:55:14 -0300

    mauriciofaustino, @vnasc: refactoring

--- src/game_of_life/core.clj
+++ src/game_of_life/core.clj
@@ -4,29 +4,31 @@
 (def rows 3)
 (def cols 3)
 
-(def neighbors-rel-pos [[-1 -1] [-1  0] [-1  1]
-                        [ 0 -1]         [ 0  1]
-                        [ 1 -1] [ 1  0] [ 1  1]])
-
 (defn- valid-position? [[row col]]
   (and (> row -1)
        (> col -1)
        (< row cols)
        (< col rows)))
 
-(defn- abs-positions [row col]
-  (filter valid-position?
-    (map (fn [[rel-row rel-col]] [(+ rel-row row) (+ rel-col col)]) neighbors-rel-pos)))
+(defn- get-neighbors-positions [row col]
+  (let [up (dec row)
+        left (dec col)
+        down (inc row)
+        right (inc col)]
+    (filter valid-position? [[up left]  [up col]  [up right]
+                              [row left]           [row right]
+                              [down left][down col][down right]])))
 
 (defn- get-neighbors [generation row col]
-  (map (fn [[abs-row abs-col]] ((generation abs-row) abs-col)) (abs-positions row col)))
+  (map (fn [[nrow ncol]] ((generation nrow) ncol))
+       (get-neighbors-positions row col)))
 
 (defn lives? [generation row col]
   (let [cell ((generation row) col)
         neighbors (get-neighbors generation row col)
         neighbors-alive (count (filter true? neighbors))]
     (or
-     (and cell
+ (and cell
           (> neighbors-alive 1)
           (< neighbors-alive 4))
      (= neighbors-alive 3))))
